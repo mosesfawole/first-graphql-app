bde04eb14ddd0a57997247cede1f43036b8539d7
Moses Fawole <mosesfawole2001@gmail.com>
2022-08-10 14:19:41 +0100

    client rendering

--- src/pages/CharactersList.jsx
+++ src/pages/CharactersList.jsx
@@ -15,8 +15,22 @@ const GET_CHARACTERS = gql`
 const CharactersList = () => {
   const { error, loading, data } = useQuery(GET_CHARACTERS);
   console.log({ error, loading, data });
+  if (loading) return <p>Loading...</p>;
 
-  return <div>CharactersList</div>;
+  if (error) return <p>Error : {error.message}</p>;
+
+  return (
+    <div className="CharacterList">
+      {data.characters.results.map((character) => {
+        return (
+          <div className="" key={character.id}>
+            <img src={character.image} alt={character.name} />
+            <h5>{character.name}</h5>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default CharactersList;
