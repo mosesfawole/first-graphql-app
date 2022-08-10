e322329282d975b8ec485369f9429ece50a66da0
Moses Fawole <mosesfawole2001@gmail.com>
2022-08-10 13:27:10 +0100

    connect React application to graphql server

--- src/index.js
+++ src/index.js
@@ -2,10 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 // https://rickandmortyapi.com/graphql
+
+const client = new ApolloClient({
+  url: "https://rickandmortyapi.com/graphql",
+  cache: new InMemoryCache(),
+});
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
