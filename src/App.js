5c0e63d14687ecf91fd5dbf875d6053ac1ecf218
Moses Fawole <mosesfawole2001@gmail.com>
2022-08-10 18:17:00 +0100

    lazy queries

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import CharactersList from "./pages/CharactersList";
 import { Routes, Route } from "react-router-dom";
 import Character from "./pages/Character";
+import Search from "./pages/Search";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route strict exact path="/" element={<CharactersList />} />
+        <Route exact path="/" element={<CharactersList />} />
+        <Route exact path="/search" element={<Search />} />
         <Route path="/:id" element={<Character />} />
       </Routes>
     </div>
---
+++ src/pages/Search.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import { gql, useLazyQuery } from "@apollo/client/";
+const GET_CHARACTER_LOCATIONS = gql`
+  query getCharacterLocations($name: String) {
+    characters(filter: { name: $name }) {
+      results {
+        location {
+          name
+        }
+      }
+    }
+  }
+`;
+
+const Search = () => {
+  const [name, setName] = useState("");
+
+  const [getLocations, { loading, error, data, called }] = useLazyQuery(
+    GET_CHARACTER_LOCATIONS,
+    {
+      variables: {
+        name,
+      },
+    }
+  );
+
+  console.log({
+    called,
+    loading,
+    error,
+    data,
+  });
+  return (
+    <div>
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <button onClick={() => getLocations()}>Search</button>
+
+      {loading && <div>Loading ...</div>}
+      {error && <div>Something went wrong</div>}
+      {data && (
+        <ul>
+          {data.characters.results.map((character) => {
+            return <li key={character.id}>{character.location.name}</li>;
+          })}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default Search;
